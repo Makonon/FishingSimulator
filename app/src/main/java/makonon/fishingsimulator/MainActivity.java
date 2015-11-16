437aa4c665c76112d1d1407f389ef1396ff4fffe
makonon <militaire.non@gmail.com>
2015-11-17 08:58:07 +0900

    Initial commit

---
+++ app/src/main/java/makonon/fishingsimulator/MainActivity.java
@@ -0,0 +1,14 @@
+package makonon.fishingsimulator;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
